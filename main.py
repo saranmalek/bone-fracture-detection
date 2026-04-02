a4578e1dfe2a169792407534d7b7306e322a9fe4
saranmalek <saranmalek439@gmail.com>
2026-04-02 18:47:18 +0530

    initial commit

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app.register_blueprint(routes)
 @app.route("/")
 def home():
     return render_template("index.html")
-
+    
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
