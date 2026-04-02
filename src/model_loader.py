997fab4da8d3b91081c0fbd285add40e0bc2b072
SAHRANKHAN S. MALEK <saranmalek439@gmail.com>
2026-04-02 20:29:13 +0530

    Update model_loader.py

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
+from tensorflow.keras.models import load_model as keras_load_model
 
 MODEL_PATH = "models/fracture_model.h5"
 
 def load_model():
-    model = tf.keras.models.load_model(
+    model = keras_load_model(
         MODEL_PATH,
-        compile=False
+        compile=False,
+        safe_mode=False
     )
     return model
